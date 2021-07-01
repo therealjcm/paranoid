92d5c2aa4eb8e5c75047e756e757bc3457c98f35
Jason Messer <jasoncmesser@gmail.com>
2021-07-01 14:54:02 -0700

    implemented generic entity, render function, game map

--- actions.py
+++ actions.py
@@ -1,8 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 class Action:
-    pass
+    def perform(self, engine: Engine, entity) -> None:
+        """Perform this action with the actions needed to determine its scope
+        `engine` is the scope this action is being performed in
+        `entity` is object performing this action
+        this action must be overriden by Action subclass
+        """
+        raise NotImplementedError()
 
 class EscapeAction(Action):
-    pass
+    def perform(self, engine: Engine, entity) -> None:
+        raise SystemExit()
 
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
@@ -10,3 +25,14 @@ class MovementAction(Action):
 
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(dest_x, dest_y):
+            return
+        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+            return
+
+        entity.move(self.dx, self.dy)
