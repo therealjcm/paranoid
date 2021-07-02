ef12a5be5ad524e7fd62d9d060030b67c987aea1
Jason Messer <jasoncmesser@gmail.com>
2021-07-02 00:41:29 -0700

    added basic field of view

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from input_handlers import EventHandler
@@ -14,6 +15,7 @@ class Engine:
         self.event_handler = event_handler
         self.player = player
         self.game_map = game_map
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -24,11 +26,25 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov()
+
+    def update_fov(self) -> None:
+        """recompute the visible area based on the player's point of view"""
+        self.game_map.visible[:] = compute_fov(
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=4
+        )
+        # if a tile is visible then it should be added to explored
+        self.game_map.explored |= self.game_map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # only print entities that are in the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
--- game_map.py
+++ game_map.py
@@ -9,9 +9,23 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
+        self.visible = np.full((width, height), fill_value=False, order="F")    # currently in FOV
+        self.explored = np.full((width, height), fill_value=False, order="F")   # ever in FOV
+
     def in_bounds(self, x: int, y: int) -> bool:
         """return true if x and y are inside of the bounds of this map"""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Renders the map
+        If a tile is in the visible array then draw it with light colors
+        if it is explored, but is not visible then draw it with dark colors
+        otherwise default to SHROUD
+        """
+
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
